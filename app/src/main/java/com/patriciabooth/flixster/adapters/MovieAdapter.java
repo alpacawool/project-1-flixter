276d8f9af0f359587c9773aaa244799ea1dc24d7
alpacawool <44758340+alpacawool@users.noreply.github.com>
2020-09-20 16:52:09 -0700

    Minor color and alignment changes for stylistic changes

--- app/src/main/java/com/patriciabooth/flixster/adapters/MovieAdapter.java
+++ app/src/main/java/com/patriciabooth/flixster/adapters/MovieAdapter.java
@@ -12,6 +12,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CenterCrop;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.bumptech.glide.request.RequestOptions;
 import com.patriciabooth.flixster.R;
 import com.patriciabooth.flixster.models.Movie;
